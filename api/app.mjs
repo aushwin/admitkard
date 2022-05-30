0b04b8953566114d5bff596911d6ae4b263c280f
aushwin <aushwin7biju@gmail.com>
2022-05-30 05:48:22 +0530

    port

--- api/app.mjs
+++ api/app.mjs
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import { dbConnect } from './app/utils/db.mjs'
 import userRouter from './app/routes/user.router.mjs'
+const port = process.env.PORT || 1000
 const {json} = express
 
 const app = express()
@@ -12,8 +13,8 @@ app.use('/users',userRouter)
 dbConnect()
     .then(()=>{
         console.log('db connected')
-        app.listen(4000,()=>{
-            console.log('server starting ')
+        app.listen(port,()=>{
+            console.log(`server starting at ${port}`)
         })
     })
     .catch((err)=>console.log(err))
